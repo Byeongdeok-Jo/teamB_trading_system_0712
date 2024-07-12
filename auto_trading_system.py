4481f8fde149cf526bac8862bed68cb04b9f4995
junhyeok-park <parkerk4@naver.com>
2024-07-12 16:08:02 +0900

    implement checking codes

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -26,15 +26,19 @@ class AutoTradingSystem:
         self._stock_driver.login(id, passwd)
 
     def buy(self, stock_code, price, count):
+        self._check_correct_code(stock_code)
         self._stock_driver.buy(stock_code, price, count)
 
     def sell(self, stock_code, price, count):
+        self._check_correct_code(stock_code)
         self._stock_driver.sell(stock_code, price, count)
 
     def get_price(self, stock_code) -> int:
+        self._check_correct_code(stock_code)
         return self._stock_driver.get_price(stock_code, 0)
 
     def buy_nice_timing(self, stock_code, budget):
+        self._check_correct_code(stock_code)
         if self._is_nice_time_to_buy(stock_code):
             self._buy_up_to_budget(budget, stock_code)
 
@@ -44,6 +48,7 @@ class AutoTradingSystem:
         self._stock_driver.buy(stock_code, current, buy_count)
 
     def sell_nice_timing(self, stock_code, count):
+        self._check_correct_code(stock_code)
         if self._is_nice_timing_to_sell(stock_code):
             self._stock_driver.sell(stock_code, self.get_price(stock_code), count)
 
@@ -63,3 +68,24 @@ class AutoTradingSystem:
 
     def get_current_budget(self):
         return (0, 0)
+
+    def _check_correct_code(self, code):
+        if not self._is_correct_code(code):
+            raise Exception(code)
+
+    def _is_correct_code(self, code):
+        length = len(code)
+        if length != 6 and length != 7:
+            return False
+        start_idx = 0
+        if length == 7:
+            if not self._is_first_char_possible(code[0]):
+                return False
+            start_idx = 1
+        all_digit = True
+        for i in range(6):
+            all_digit = all_digit and code[i + start_idx].isdigit()
+        return all_digit
+
+    def _is_first_char_possible(self, char):
+        return char == 'A' or char == 'B' or char == 'C' or char == 'K'
