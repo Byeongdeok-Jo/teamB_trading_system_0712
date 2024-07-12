0499a3af118c92ff5f541ea0cfceb2012e4521e0
jihun75.kim <gnswlrla0218@naver.com>
2024-07-12 13:43:01 +0900

    feature Mock_driver

--- Mock_driver.py
+++ Mock_driver.py
@@ -8,16 +8,17 @@ class MockDriver(IStockDriver):
         self.mock_price = 5500
 
     def login(self, id, password):
-        pass
+        self.logged_in = True
+        self.actions.append(f"Logged in as {id}")
 
     def buy(self, stock_code, price, count):
-        pass
+        self.actions.append(f"Bought {count} of {stock_code} at {price}")
 
     def sell(self, stock_code, price, count):
-        pass
+        self.actions.append(f"Sold {count} of {stock_code} at {price}")
 
     def get_price(self, stock_code, minute=0):
-        pass
+        return self.mock_price
 
     def set_mock_price(self, price):
-        pass
+        self.mock_price = price
